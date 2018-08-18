037b9b4bce139efe3135ed4a2eb85171db793ae1
sathyaraj819 <40590405+sathyaraj819@users.noreply.github.com>
2018-08-18 09:27:11 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.applet.*;
+public class square extends Applet
+{
+  public void paint(Graphics g)
+  {
+    g.setColor(Color.blue);
+    g.drawRect(100, 100, 100, 100);
+   // g.setColor(Color.magenta);
+  }
+}
